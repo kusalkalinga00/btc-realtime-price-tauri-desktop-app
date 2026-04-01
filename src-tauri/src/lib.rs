c45a7534c3661bce7e56d0b6fcb5a59e10c21226
Kusal Kalinga <kusalkalingainfo@gmail.com>
2026-04-01 11:20:24 +0530

    feat: enhance tray icon functionality with event handling and window management

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,57 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{
+    Emitter, Manager, menu::{Menu, MenuItem}, tray::{MouseButton, MouseButtonState, TrayIconBuilder }
+};
+use tauri_plugin_positioner::{Position, WindowExt};
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            let _ = window.hide();
+            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+            let menu = Menu::with_items(app, &[&quit])?;
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, Some(14.0))
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            TrayIconBuilder::with_id("main")
+                .title("Click to open")
+                .menu(&menu)
+                .show_menu_on_left_click(false).on_tray_icon_event(|tray_handle, event| {
+                tauri_plugin_positioner::on_tray_event(tray_handle.app_handle(), &event);
+
+
+                if let tauri::tray::TrayIconEvent::Click {
+                    button: MouseButton::Left,
+                    button_state: MouseButtonState::Up,
+                    ..
+                } = event
+                {
+                    let app = tray_handle.app_handle();
+                    if let Some(window) = app.get_webview_window("main") {
+                            if window.is_visible().unwrap_or(false) {
+                                let _ = window.hide();
+                            } else {
+                                let _ = window
+                                    .as_ref()
+                                    .window()
+                                    .move_window(Position::TrayBottomCenter);
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }
+                }
+              })
+                .build(app)?;
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
